8f33e7a8ddec7d9c0dbbb687456391c32998c10e
yaroslav-ddt <88374855+yaroslav-ddt@users.noreply.github.com>
2021-10-29 15:10:01 +0200

    Update staticwebapp.tf

--- terraform/staticwebapp.tf
+++ terraform/staticwebapp.tf
@@ -2,7 +2,7 @@ resource "azurerm_static_site" "swa" {
   name                = "${var.resource_name}"
   location            = azurerm_resource_group.swa_rg.location
   resource_group_name = azurerm_resource_group.swa_rg.name
-  sku_tier = "Free" #possible values are "Free" or "Standard"
+  sku_tier = "Standard" #possible values are "Free" or "Standard"
   tags                = {
     "owner" = "yy"
   }
